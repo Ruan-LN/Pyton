270872ad2b93b294d030f44edd627a54c3de9563
Ruan Lima <ruanlima933@gmail.com>
2021-09-02 19:44:08 -0300

    commit inicial

---
+++ Hello, Word.py
@@ -0,0 +1,15 @@
+print ("Hello, Word");
+# Básico / Basic " Hello Word"
+
+
+# Avançado com Fução / Advanceer wich funtion 
+
+def hello ():
+    print(" Hello, Word (def)");
+
+hello ()
+
+def code ():
+    print (" 1° Code at Pyton / 1° codigo em pyton")
+
+code ()
