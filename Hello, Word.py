0206c671644ca779fea39c78c4224d41a135f880
Ruan Lima <ruanlima933@gmail.com>
2021-09-02 20:09:55 -0300

    Interação báscia com usuário

--- Hello, Word.py
+++ Hello, Word.py
@@ -5,11 +5,14 @@ print ("Hello, Word");
 # Avançado com Fução / Advanceer wich funtion 
 
 def hello ():
-    print(" Hello, Word (def)");
+    print(" Hello, Word (def)")
 
 hello ()
 
 def code ():
-    print (" 1° Code at Pyton / 1° codigo em pyton")
+    print(" 1° Code at Pyton / 1° codigo em pyton")
 
 code ()
+
+nome = input("Digite seu nome:")
+print("Hello Word!_"+nome)
